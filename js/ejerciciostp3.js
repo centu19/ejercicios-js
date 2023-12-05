eef018484e5e4f82ae201d9ffd266ed4ac74025d
centu19 <estebancenturion4@hotmail.com>
2023-12-05 10:00:57 -0300

    archivo del tp3

---
+++ js/ejerciciostp3.js
@@ -0,0 +1,7 @@
+// ejercicio 1
+ let meses = ["Enero","Febrero","Marzo","Abril","Mayo","junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+ console.log('El array original-->', meses);
+
+let mesesOrdenados = meses.forEach(element => {FileList
+    
+});
